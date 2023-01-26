225521adc1e37f766370937f657692219593150f
Paulo Alvim <pauloalvim@gmail.com>
2023-01-26 01:40:07 -0300

    Adds new constants to help with development.

---
+++ lib/shared/figma_colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class FigmaColors {
+  static const Color verdeCallToAction = Color(0xFF5B7625);
+  static const Color terraRoxa = Color(0xFF816267);
+  static const Color laranjaManutencao = Color(0xFFFF650E);
+  static const Color graficoLinhaCor = Color(0xFFFFB800);
+  static const Color verdeVida = Color(0xFF7FA92C);
+  static const Color vermelhoTresSprays = Color(0xFF9E3131);
+  static const Color verdeZeroSprays = Color(0xFF319E49);
+  static const Color amareloDoisSprays = Color(0xFFA4A043);
+  static const Color azulUmSpray = Color(0xFF467BB9);
+  static const Color verdeOperacao = Color(0xFF00FF0A);
+  static const Color cinzaDesativado = Color(0xFF9B9B9B);
+  static const Color cinzaClaroDesativado = Color(0xFFB0AFAF);
+  static const Color terraEscura = Color(0xFF1E1D1E);
+  static const Color filtroDeMapa = Color(0xBD1F212B);
+}
